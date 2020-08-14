0daa6fdd3f40a722454e16e08f0be11b567080c1
DDDDxh <244012506@qq.com>
2020-08-14 15:39:52 +0800

    合并了

--- hrm/src/com/dxh/hrm/test/Test.java
+++ hrm/src/com/dxh/hrm/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		System.out.println("test------------------------------");
 	}
 	public void test() {
-		System.out.println("woooooohaaaaaa");
+		System.out.println("wooohaaaaaa");
 	}
 
 }
